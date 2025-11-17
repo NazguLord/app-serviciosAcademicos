f5eefedd07a5ac902cc334d4a7d65a9647a09a5f
NazguLord <elierec2@hotmail.com>
2025-11-17 08:25:40 -0600

    33. Agregar seleccion Desarrollo - Produccion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//Producción
+/*
 export default defineConfig(({ command }) => ({
   base: '/sv/' ,
   plugins: [react()],
@@ -11,6 +13,23 @@ export default defineConfig(({ command }) => ({
     allowedHosts: ['registro.cp.unicah.net'],
     hmr: {
       host: 'registro.cp.unicah.net',
+      protocol: 'ws'
+    }
+  }
+}));
+*/
+
+// Desarollo
+export default defineConfig(({ command }) => ({
+  base: '/sv/' ,
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    origin: 'http://unicahdev.registro.cp.unicah.edu',
+    allowedHosts: ['unicahdev.registro.cp.unicah.edu'],
+    hmr: {
+      host: 'unicahdev.registro.cp.unicah.edu',
       protocol: 'ws'
     }
   }
